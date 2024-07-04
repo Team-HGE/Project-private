b65bd1bdde27c337571267d7caa2170c74685a93
attiya1125 <leonrey1125@gmail.com>
2024-07-04 10:19:02 +0900

    [Update] Scene_07

--- Assets/Scripts/LJM_MapObject/DoorObject.cs
+++ Assets/Scripts/LJM_MapObject/DoorObject.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class DoorObject : InteractableObject
 {
-    [SerializeField] Animator animator;
+    [SerializeField] bool isOpen;
+    [SerializeField] bool interactableOneTime;
+    [SerializeField] Vector3 openRotate;
+    [SerializeField] Vector3 closeRotate;
+    [SerializeField] Transform parentObject;
     public override void ActivateInteraction()
     {
         if (isInteractable) return;
@@ -13,8 +18,23 @@ public class DoorObject : InteractableObject
 
     public override void Interact()
     {
-        isInteractable = true;
-        animator = GetComponent<Animator>();
-        animator.SetBool("isOpen", true);
+        if (interactableOneTime)
+        {
+            isInteractable = true;
+        }
+
+        Vector3 targetRotate;
+        if (isOpen )
+        {
+            targetRotate = closeRotate;
+            isOpen = false;
+        }
+        else
+        {
+            targetRotate = openRotate;
+            isOpen = true;
+        }
+        parentObject.DOKill();
+        parentObject.DORotate(targetRotate, 2f);
     }
 }
