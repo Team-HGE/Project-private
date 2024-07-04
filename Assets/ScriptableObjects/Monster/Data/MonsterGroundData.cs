742efa7f274dad7f4a243d0d5b8ca6b029a627d0
leemoojin <hzm_deli@naver.com>
2024-07-04 14:08:01 +0900

    [Update] Scene2

--- Assets/ScriptableObjects/Monster/Data/MonsterGroundData.cs
+++ Assets/ScriptableObjects/Monster/Data/MonsterGroundData.cs
@@ -7,8 +7,8 @@ public class MonsterGroundData
     [field: Header("BaseData")]
     [field: SerializeField][field: Range(0f, 25f)] public float BaseSpeed { get; private set; } = 5f;
     [field: SerializeField][field: Range(0f, 25f)] public float BaseRotationDamping { get; private set; } = 1f;
-    [field: SerializeField][field: Range(0f, 20f)] public float PlayerChasingRange { get; private set; } = 5f;
-    [field: SerializeField][field: Range(0f, 5f)] public float AttackRange { get; private set; } = 1.5f;
+    [field: SerializeField][field: Range(0f, 30f)] public float PlayerChasingRange { get; private set; } = 10f;
+    [field: SerializeField][field: Range(0f, 10f)] public float AttackRange { get; private set; } = 1.5f;
     [field: SerializeField][field: Range(0f, 180f)] public float ViewAngle { get; private set; } = 120f;
 
 
@@ -16,14 +16,14 @@ public class MonsterGroundData
 
 
     [field: Header("PatrolData")]
-    [field: SerializeField][field: Range(0f, 10f)] public float PatrolSpeed { get; private set; } = 1f;
-    [field: SerializeField][field: Range(0f, 10f)] public float PatrolRange { get; private set; } = 7f;
+    [field: SerializeField][field: Range(0f, 10f)] public float PatrolSpeed { get; private set; } = 2f;
+    [field: SerializeField][field: Range(0f, 25f)] public float PatrolRange { get; private set; } = 15f;
 
     [field: Header("FindData")]
     [field: SerializeField][field: Range(0f, 5f)] public float FindTransitionTime { get; private set; } = 1.5f;
 
     [field: Header("ChaseData")]
-    [field: SerializeField][field: Range(0f, 10f)] public float ChaseSpeed { get; private set; } = 2.5f;
+    [field: SerializeField][field: Range(0f, 10f)] public float ChaseSpeed { get; private set; } = 4f;
 
     [field: Header("LoseSightData")]
     [field: SerializeField][field: Range(0f, 5f)] public float LoseSightTransitionTime { get; private set; } = 1.5f;
