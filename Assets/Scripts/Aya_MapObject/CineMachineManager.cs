73fe51061957b2dc1176f034a598511ebccb959d
attiya1125 <leonrey1125@gmail.com>
2024-08-05 10:53:12 +0900

    [Update] ProBuilderScene2_19

--- Assets/Scripts/Aya_MapObject/CineMachineManager.cs
+++ Assets/Scripts/Aya_MapObject/CineMachineManager.cs
@@ -1,8 +1,5 @@
 using Cinemachine;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CinemachineManager : MonoBehaviour
