89684f73084d8486e9f0eb67b0f9a4c29b9a229c
attiya1125 <leonrey1125@gmail.com>
2024-08-12 00:51:02 +0900

    [Update] ElevatorTimeLine

--- Assets/Scripts/Aya_MapObject/ElevatorTimeLine.cs
+++ Assets/Scripts/Aya_MapObject/ElevatorTimeLine.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -8,6 +9,11 @@ public class ElevatorTimeLine : MonoBehaviour
     public GameObject timeLineObject;
     public GameObject UI;
     public Light playerLight;
+
+    public TextMeshPro roomTxt101;
+    public TextMeshPro roomTxt102;
+    public TextMeshPro roomTxt101_111;
+    public TextMeshPro roomTxt112_121;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,5 +31,18 @@ public class ElevatorTimeLine : MonoBehaviour
         playerLight.enabled = false;
         UI.SetActive(false);
         timelineDirector.Play();
+    }
+
+    public void OffLight()
+    {
+        GameManager.Instance.lightManager.OffLaversAllLight();
+    }
+
+    public void ChangeTxt()
+    {
+        roomTxt101.text = "2";
+        roomTxt102.text = "2";
+        roomTxt101_111.text = "201 - 211";
+        roomTxt112_121.text = "212 - 221";
     }
 }
