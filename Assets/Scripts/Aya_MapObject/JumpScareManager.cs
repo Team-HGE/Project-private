346e64bf55a4bb10417b01091b4f462550b1fc6a
attiya1125 <leonrey1125@gmail.com>
2024-08-12 14:25:26 +0900

    [Update] Hotel_Day1, 2_4

--- Assets/Scripts/Aya_MapObject/JumpScareManager.cs
+++ Assets/Scripts/Aya_MapObject/JumpScareManager.cs
@@ -3,17 +3,23 @@ using Cinemachine;
 using UnityEngine.AI;
 using DG.Tweening;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
 public enum JumpScareType
 {
+    GroupTypeMonster,
     EyeTypeMonster,
     EarTypeMonster
+}
+[Serializable]
+public class MonstersJumpScare
+{
+    public JumpScareType jumpScareType;
+    public GameObject gameObject;
 }
 public class JumpScareManager : MonoBehaviour
 {
     public static JumpScareManager Instance;
-
-    [Header("VC")]
-    public Transform vcFollow;
 
     [Header("FlashLight")]
     public GameObject flashLight;
@@ -21,9 +27,14 @@ public class JumpScareManager : MonoBehaviour
     [Header("Death")]
     public GameObject deathCanvas;
 
-    public AudioClip[] jumpScareAudioClips;
-
     [Header("MonsterControllers")]
-    [SerializeField] NavMeshAgent[] monstersNavMeshAgent;
-    
+    public NavMeshAgent[] monstersNavMeshAgent;
+
+    [Header("MonsterType")]
+    public MonstersJumpScare[] monstersJumpScare;
+
+    public void PlayJumpScare(JumpScareType jumpScareType)
+    {
+        monstersJumpScare[(int)jumpScareType].gameObject.SetActive(true);
+    }
 }
