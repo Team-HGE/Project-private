823945a78bc4f1b4ee6b7d68108f6a2497d887a0
attiya1125 <leonrey1125@gmail.com>
2024-08-12 10:46:00 +0900

    [Add] AlienAnimation

--- Assets/Scripts/Aya_MapObject/LightManager.cs
+++ Assets/Scripts/Aya_MapObject/LightManager.cs
@@ -28,18 +28,18 @@ public class LightManager : MonoBehaviour
     public List<MeshRenderer> lobbyObjectRenderer = new List<MeshRenderer>();
 
     [Header("UseLights")]
-    public Material Use_Y_Lights;
-    public Material Use_W_Lights;
-    public Material Use_WY_Lights;
-    public Material Use_Week_Lights;
-    public Material Use_Bar_Lights;
+    [SerializeField] Material Use_Y_Lights;
+    [SerializeField] Material Use_W_Lights;
+    [SerializeField] Material Use_WY_Lights;
+    [SerializeField] Material Use_Week_Lights;
+    [SerializeField] Material Use_Bar_Lights;
 
     [Header("noneLights")]
-    public Material None_Y_Lights;
-    public Material None_W_Lights;
-    public Material None_WY_Lights;
-    public Material None_Week_Lights;
-    public Material None_Bar_Lights;
+    [SerializeField] Material None_Y_Lights;
+    [SerializeField] Material None_W_Lights;
+    [SerializeField] Material None_WY_Lights;
+    [SerializeField] Material None_Week_Lights;
+    [SerializeField] Material None_Bar_Lights;
 
     private Dictionary<string, LightName> materailLightName = new Dictionary<string, LightName>()
     {
@@ -178,6 +178,42 @@ public class LightManager : MonoBehaviour
             }
         }
     }
+    public void OnChangeMaterial(List<MeshRenderer> meshRenderers)
+    {
+        foreach (var renderer in meshRenderers)
+        {
+            if (renderer == null)
+            {
+                continue;
+            }
+            Material[] newMaterial = renderer.materials;
+            for (int i = 0; i < newMaterial.Length; i++)
+            {
+                string materialName = newMaterial[i].name.Replace(" (Instance)", "");
+                LightName newName = GetMaterialType(materialName);
+                switch (newName)
+                {
+                    case LightName.None_Bar_Lights:
+                        newMaterial[i] = Use_Bar_Lights;
+                        break;
+                    case LightName.None_Y_Lights:
+                        newMaterial[i] = Use_Y_Lights;
+                        break;
+                    case LightName.None_WY_Lights:
+                        newMaterial[i] = Use_WY_Lights;
+                        break;
+                    case LightName.None_W_Lights:
+                        newMaterial[i] = Use_W_Lights;
+                        break;
+                    case LightName.None_Week_Lights:
+                        newMaterial[i] = Use_Week_Lights;
+                        break;
+                    default: break;
+                }
+                renderer.materials = newMaterial;
+            }
+        }
+    }
     LightName GetMaterialType(string materialName)
     {
         if (materailLightName.TryGetValue(materialName, out LightName materialType))
@@ -185,27 +221,5 @@ public class LightManager : MonoBehaviour
             return materialType;
         }
         return LightName.Unknown;
-    }
-    float time;
-    bool lightOff;
-    private void Update()
-    {
-        if (!lightOff)
-        {
-            time += Time.deltaTime;
-            if (time > 10)
-            {
-                lightOff = true;
-                HotelFloorScene_DataManager.Instance.controller.isCentralPowerActive = false;
-                //OffLaversAllLight();
-                OffListLight(lobbyLights);
-                OffChangeMaterial(lobbyObjectRenderer);
-                
-               // foreach (var obj in HotelFloorScene_DataManager.Instance.controller.barrierObjects)
-                //{
-                 //   obj.CloseAni();
-                //}
-            }
-        }
     }
 }
