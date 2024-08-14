4581c953e877a710995c2bf5024dde855c3e3f70
attiya1125 <leonrey1125@gmail.com>
2024-08-14 17:51:37 +0900

    [Update] Hotel_Day2

--- Assets/Scripts/Aya_MapObject/RandomEventCollider.cs
+++ Assets/Scripts/Aya_MapObject/RandomEventCollider.cs
@@ -6,12 +6,50 @@ public class RandomEventCollider : MonoBehaviour
 {
     [SerializeField] GameObject eventObject;
     [SerializeField] bool isTrigger;
+    [SerializeField] float delayTime = 0;
+
+    [SerializeField] BreakableWindow[] glass;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && !isTrigger)
         {
             isTrigger = true;
-            eventObject.SetActive(true);
+
+            int rand = Random.RandomRange(0, 100);
+
+            if (rand > 100)
+            {
+                eventObject.SetActive(true);
+            }
+            else
+            {
+                foreach (var c in glass)
+                {
+                    if (c.isBroken || c == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        c.breakWindow();
+                        break;
+                    }
+                }
+            }
+
+            StartCoroutine(EventDelay());
         }
+    }
+
+    IEnumerator EventDelay()
+    {
+        while (delayTime < 60)
+        {
+            delayTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isTrigger = false;
+        delayTime = 0;
     }
 }
